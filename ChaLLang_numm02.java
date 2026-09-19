d9098a7490c36a45d83d35a464e191148f011795
BenZic__Second_acc_Windows <benziccc@gmail.com>
2026-09-19 20:01:57 +0800

    fix the output and upgrade the system.

--- ChaLLang_numm02.java
+++ ChaLLang_numm02.java
@@ -16,12 +16,15 @@ public class ChaLLang_numm02 {
             age = s.nextInt();
 
         if (age >= 1 && age < 18 ){
-            System.out.print("\n\t ---Sorry you not allow a vote of election\n\t   Because this is 18+ only in voting");
+            System.out.print("\n\t ---Sorry you not allow a vote of election\n\t   - Because this is 18+ only in voting.\n\n");
         }
         
         if(age >= 18){
 
             s.nextLine();
+
+            System.out.println("\n\t\t|#VOTE Now!!!|");
+            System.out.println(" | ako si dogie 3    ||||     BenZic      |\n\n");
 
             System.out.print("\nenter your nam: ");
             mayorName = s.nextLine();
@@ -29,10 +32,12 @@ public class ChaLLang_numm02 {
                 if(mayorName.equalsIgnoreCase("ako si dogie 3")){
 
                     System.out.printf("\n\n   --- you vote is sucssesfuly for voting to[ %s ] !!!\n",mayorName);
+                    System.out.print("\n\n\tThank You for voting in my  --- Voting System. V1 ---\n\n");
 
                 }else if(mayorName.equalsIgnoreCase("BenZic")){
 
                     System.out.printf("\n\n   --- you vote is sucssesfuly for voting to[ %s ] !!!\n",mayorName);
+                    System.out.print("\n\n\tThank You for voting in my  --- Voting System. V1 ---\n\n");
 
                 } else{
                     System.out.print("\n\nError please repeat\n");
@@ -40,7 +45,7 @@ public class ChaLLang_numm02 {
         
             }
 
-            System.out.print("\n\n\tThank You for voting in my  --- Voting System. V1 ---");
+            
         
 
 
